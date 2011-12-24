5b6142cde7acd22ea1f7cdad39d8463844c599cb
mathieu duponchelle <duponc_m@Meh.(none)>
2011-12-24 20:28:17 +0100

    Add to string to check if we have "text" in the stream caps.

--- pitivi/ui/common.py
+++ pitivi/ui/common.py
@@ -131,7 +131,7 @@ def beautify_stream(stream):
             templ = templ % (par * stream.get_height(), stream.get_height())
         return templ
     elif type(stream) == gst.pbutils.DiscovererStreamInfo and\
-             "text" in  stream.get_caps():
+             "text" in  stream.get_caps().to_string():
         return _("Subtitles")
     raise NotImplementedError
 
